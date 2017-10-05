462eb86d5d0f6a36050dad176450985bd712718e
Argyris Zymnis <argyris.zymnis@zee.aero>
2017-10-05 16:17:10 -0700

    Define actions

---
+++ frontend/src/actions/index.js
@@ -0,0 +1,96 @@
+// Post actions
+export const CREATE_POST    = "CREATE_POST"
+export const DELETE_POST    = "DELETE_POST"
+export const EDIT_POST      = "EDIT_POST"
+export const UP_VOTE_POST   = "UP_VOTE_POST"
+export const DOWN_VOTE_POST = "DOWN_VOTE_POST"
+
+export function createPost({id, timestamp, title, body, author, category}) {
+  return {
+    type: CREATE_POST,
+    id,
+    timestamp,
+    title,
+    body,
+    author,
+    category
+  }
+}
+
+export function deletePost(id) {
+  return {
+    type: DELETE_POST,
+    id
+  }
+}
+
+export function editPost({id, title, body}) {
+  return {
+    type: EDIT_POST,
+    id,
+    title,
+    body
+  }
+}
+
+export function upVotePost(id) {
+  return {
+    type: UP_VOTE_POST,
+    id
+  }
+}
+
+export function downVotePost(id) {
+  return {
+    type: DOWN_VOTE_POST,
+    id
+  }
+}
+
+// Comment actions
+export const CREATE_COMMENT    = "CREATE_COMMENT"
+export const DELETE_COMMENT    = "DELETE_COMMENT"
+export const EDIT_COMMENT      = "EDIT_COMMENT"
+export const UP_VOTE_COMMENT   = "UP_VOTE_COMMENT"
+export const DOWN_VOTE_COMMENT = "DOWN_VOTE_COMMENT"
+
+export function createComment({id, timestamp, body, author, parentId}) {
+  return {
+    type: CREATE_COMMENT,
+    id,
+    timestamp,
+    body,
+    author,
+    parentId
+  }
+}
+
+export function deleteComment(id) {
+  return {
+    type: DELETE_COMMENT,
+    id
+  }
+}
+
+export function editComment({id, title, body}) {
+  return {
+    type: EDIT_COMMENT,
+    id,
+    title,
+    body
+  }
+}
+
+export function upVoteComment(id) {
+  return {
+    type: UP_VOTE_COMMENT,
+    id
+  }
+}
+
+export function downVoteComment(id) {
+  return {
+    type: DOWN_VOTE_COMMENT,
+    id
+  }
+}
