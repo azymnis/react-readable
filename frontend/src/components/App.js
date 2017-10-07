2356af679160a7fd7ccc0249c00390414fb5b9c8
Argyris Zymnis <argyris.zymnis@zee.aero>
2017-10-06 20:05:45 -0700

    Create a method that generates initial state

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -2,10 +2,9 @@ import React, { Component } from 'react';
 import * as BackendAPI from '../utils/api.js'
 
 class App extends Component {
-
   componentDidMount() {
     BackendAPI.getAllCategories().then(data => console.log(data))
-    BackendAPI.getAllPosts().then(data => console.log(data))
+    BackendAPI.getInitialState().then(data => console.log(data))
   }
 
   render() {
--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -25,3 +25,28 @@ export function getAllPosts() {
 export function getAllCommentsForPost(id) {
   return getUrlToJson(`${api}/posts/${id}/comments`)
 }
+
+/**
+ * This will return a promise of an object which has two keys:
+ * posts and comments. The value of posts is a map of postId to post.
+ * Each post contains a list of all commentIds of all child comments
+ * for that post. Similarly comments is a map of commentId to comment.
+ */
+export function getInitialState() {
+  return getAllPosts().then(posts =>
+    Promise.all(posts.map(post =>
+      getAllCommentsForPost(post.id).then(comments => {
+        return { post, comments }
+      })
+    ))
+  ).then(resultList => {
+    const result = { posts: {}, comments: {}}
+    resultList.forEach (({ post, comments }) => {
+      const commentIds = comments.map(comment => comment.id)
+      post.comments = commentIds
+      result.posts[post.id] = post
+      comments.forEach(comment => result.comments[comment.id] = comment)
+    })
+    return result
+  })
+}
